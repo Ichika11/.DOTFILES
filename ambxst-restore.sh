15109b97f7153f6d73c2bfa26878aba1cc4b4b5f
ichika <pranavjirel@gmail.com>
2026-06-05 11:37:01 +0545

    ambxst backup - 2026-06-05 11:37

--- ambxst-restore.sh
+++ ambxst-restore.sh
@@ -3,12 +3,16 @@ echo "restoring ambxst rice..."
 
 pkill quickshell
 pkill swaync
+pkill -f ambxst
+sleep 1
+
 
 cp -r ~/dotfiles/ambxst/ambxst ~/.config/
 cp ~/dotfiles/ambxst/hyprland.conf ~/.local/share/ambxst/
 cp ~/dotfiles/ambxst/hyprland.lua ~/.local/share/ambxst/
 cp ~/dotfiles/ambxst/hyprland-main.conf ~/.config/hypr/hyprland.conf
 hyprctl reload
+sleep 1
 ambxst & disown
 
 echo "done! ambxst restored"
